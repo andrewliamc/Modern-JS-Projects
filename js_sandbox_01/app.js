88835cb768f9604dea0befde452c4c0da2b798e8
Andrew Liam C <andrewliamc@startmail.com>
2022-03-10 01:24:06 -0500

    Setup the first lecture sandbox
       - goes over Console object and methods

---
+++ js_sandbox_01/app.js
@@ -0,0 +1,58 @@
+// Log to Console
+
+// string 
+console.log('Hello World'); 
+
+// number
+console.log(123);
+
+// boolean
+console.log(true);
+
+// defining a variable 'greeting' to mean 'Hello'
+var greeting = 'Hello';
+
+// printing the meaning of the variable 'greeting'
+console.log(greeting);
+
+// an array
+console.log([1,2,3,4]);
+
+// a string template literal 
+// this will log the object
+console.log({a:1, b:2});
+
+// other thing that you can do with OBJECTS is display a table of the values:
+console.table({a:1, b:2});
+
+// logging errors
+console.error('This is some error');
+// this will log some error, and output it in RED
+
+// clearing the console:
+console.clear();
+
+// warnings:
+console.warn('This is a warning!');
+
+// the timer
+console.time('Hello');
+console.log('Hello World!');
+console.log('Hello World!');
+console.log('Hello World!');
+console.log('Hello World!');
+console.log('Hello World!');
+console.log('Hello World!');
+console.log('Hello World!');
+console.log('Hello World!');
+console.timeEnd('Hello');
+// This will output the time that it took from start to finish to run the script.
+
+
+/* 
+  Multi 
+  Line 
+  Comments 
+  */ 
+
+
