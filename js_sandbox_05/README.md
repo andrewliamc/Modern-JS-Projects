267efade6d92cbaf1ee38135330506f9de2d8baa
Andrew Liam C <andrewliamc@startmail.com>
2022-03-13 00:40:01 -0400

    Creating the sandbox for notes and practice.

---
+++ js_sandbox_05/README.md
@@ -0,0 +1,6 @@
+
+Math is an object.
+   - meaning that it has properties and methods
+a
+A property is an attribute.
+A method is just a function
---
+++ js_sandbox_05/app.js
@@ -0,0 +1,20 @@
+// const num1 = 100;
+// const num2 = 50;
+// let val;
+
+// // Simple math with numbers
+// val = num1 + num2;
+// val = num1 * num2;
+// val = num1 - num2;
+// val = num1 / num2;
+// val = num1 % num2;
+
+// // Math Object
+// val = Math.PI;
+
+
+// console.log(val);
+
+value = Math.floor(Math.random() * 20 + 1);
+
+console.log(value);
